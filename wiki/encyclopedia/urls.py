7bd2735bf616cf8699cd3d7aec4590d8aef7661c
André Pennini <andrepennini@usp.br>
2023-10-04 23:19:38 -0300

    css styling start

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,6 +6,7 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:entry>", views.entry_page, name="entry"),
+    path("wiki/<str:entry>", views.entry_page, name="entry"),
     path("search/", views.search, name="search"),
     path("create/", views.create, name="create"),
     path("edit/<str:entry>", views.edit, name="edit"),
