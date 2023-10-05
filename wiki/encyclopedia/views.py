14b8d0ad06ebac1e1336cf9c7064dce6806656ec
André Pennini <andrepennini@usp.br>
2023-10-04 22:21:17 -0300

    create new pages function finished

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-
 
 from . import util
 
@@ -10,25 +9,56 @@ def index(request):
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
 
-def entry_page(request, entry):
-    return render(
-        request,
-        "encyclopedia/entry.html",
-        {"entry_name": entry, "entry": util.get_entry(entry)},
-    )
-
+def entry_page(request, entry:str):
+    entries = [en.upper() for en in util.list_entries()]
+    if entry.upper() in entries:
+        return render(
+            request,
+            "encyclopedia/entry.html",
+            {"entry_name": entry, "entry": util.get_entry(entry)},
+        )
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": 404,
+            "error_message": f"The title '{entry}' does not exist."
+        })
 
 def search(request):
-    entry = request.GET.get("q").upper()
+    entry = request.GET.get("q")
     entries = util.list_entries()
-    entries_upper = [en.upper() for en in entries]
-    if entry in entries_upper:
-        return HttpResponseRedirect(reverse("encyclopedia:entry", args=[entry]))
-    else:
-        if entry:
-            result = [en for en_up, en in zip(entries_upper, entries) if entry in en_up]
+    result = []
+    if entry:
+        for en in entries:
+            if entry.upper() == en.upper():
+                return HttpResponseRedirect(reverse("encyclopedia:entry", args=[entry]))
+            if entry.upper() in en.upper():
+                result.append(en)
+    if not result:
+        return render(request, "encyclopedia/error.html", {
+            "error": 404,
+            "error_message": f"The title '{entry}' wasn't found."
+        })
+    return render(request, "encyclopedia/search.html", {"entry": result})
+
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        text = request.POST.get("textarea").encode("utf-8")
+        entries = util.list_entries()
+        exists = [en for en in entries if title.upper() == en.upper()]
+        if not exists:
+            if title and text:
+                util.save_entry(title, text)
+                return HttpResponseRedirect(reverse("encyclopedia:index"))
+            else:
+                return render(request, "encyclopedia/error.html", {
+                "error": 400,
+                "error_message": "Must provide title and Markdown text."
+            })
         else:
-            result = None
-        return render(request, "encyclopedia/search.html", {"entry": result})
-
-
+            return render(request, "encyclopedia/error.html", {
+            "error": 400,
+            "error_message": f"The title '{exists[0]}' already exists"
+        })
+    return render(request, "encyclopedia/create.html")
