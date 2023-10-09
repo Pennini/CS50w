f8086649ac7a69360d5f7a082998d87c5c4f1bfb
André Pennini <andrepennini@usp.br>
2023-10-09 18:57:54 -0300

    Models passed to admin

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import *
 
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(Bids, BidsAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Watchlist, WatchlistAdmin)
