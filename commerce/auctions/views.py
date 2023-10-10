8e55d500d3db0a6341ba5a34940802f6a77acf8e
André Pennini <andrepennini@usp.br>
2023-10-10 11:17:16 -0300

    Image media root installed

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -8,12 +8,13 @@ from django.db.models import F
 
 from .models import *
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def index(request):
-    auctions = Auction.objects.raw("SELECT * FROM auctions_auction AS a JOIN auctions_bids AS b ON a.id = b.auction_id_id")
-    return render(request, "auctions/index.html", {
-        "auction_listing": auctions
-    })
+    auctions = Auction.objects.raw(
+        "SELECT * FROM auctions_auction AS a JOIN auctions_bids AS b ON a.id = b.auction_id_id"
+    )
+    return render(request, "auctions/index.html", {"auction_listing": auctions})
 
 
 def login_view(request):
@@ -28,13 +29,16 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            return render(
+                request,
+                "auctions/login.html",
+                {"message": "Invalid username and/or password."},
+            )
     else:
         return render(request, "auctions/login.html")
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
@@ -49,41 +53,49 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+            return render(
+                request, "auctions/register.html", {"message": "Passwords must match."}
+            )
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
+            return render(
+                request,
+                "auctions/register.html",
+                {"message": "Username already taken."},
+            )
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def create_auction(request):
     if request.method == "POST":
         title = request.POST["title"]
         description = request.POST["description"]
         category = request.POST["category"]
-        image = request.POST["image"]
+        image = request.FILES["image"]
         price = request.POST["price"]
 
         if not title or not description or not price:
-            return render(request, "auctions/error.html", {
-                "error": 400,
-                "error_message": "Must provide title and description and the starting bid"
-            })
+            return render(
+                request,
+                "auctions/error.html",
+                {
+                    "error": 400,
+                    "error_message": "Must provide title and description and the starting bid",
+                },
+            )
         else:
-            auction = Auction(title=title, description=description, user_id=request.user)  
-        
+            auction = Auction(
+                title=title, description=description, user_id=request.user
+            )
+
             if image:
                 auction.image = image
             if category:
@@ -92,12 +104,6 @@ def create_auction(request):
             auction.save()
             price = Bids(auction_id=auction, starting_bid=price)
             price.save()
-            return HttpResponseRedirect(reverse('index'))
-    
+            return HttpResponseRedirect(reverse("index"))
+
     return render(request, "auctions/create.html")
-
-
-
-
-
-
