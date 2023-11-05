8f088b95691fed201bcb753f500629be293243b1
Andre <andrepennini@gmail.com>
2023-11-05 16:32:32 -0300

    adding emails

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,21 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  
-  document.querySelector('#compose-form').onsubmit = () => {
-    fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-        recipients: document.querySelector('#compose-recipients').value,
-        subject: document.querySelector('#compose-subject').value,
-        body: document.querySelector('#compose-body').value
-      })
-    })
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
-    });
-  }
+
+  document.querySelector('#compose-form').addEventListener('submit', sendEmail);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -39,10 +26,54 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
   
+  const emails = document.querySelector('#emails-view');
   // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
+  emails.style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  emails.innerHTML += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  const list = document.createElement("ul");
+  list.className = "list-group";
+  emails.append(list);
+
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+      // ... do something else with emails ...
+      for (var i = 0; i < emails.length; i++) {
+        const li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.innerHTML = `<span> id="email${i}" ${emails[i].sender}</span> <span class="subject">${emails[i].subject}</span> <span class="timestamp">${emails[i].timestamp}</span>`;
+        list.append(li);
+      }
+  });
+
+}
+
+function sendEmail(event) {
+  event.preventDefault();
+  
+  const recipients = document.querySelector("#compose-recipients").value;
+  const subject = document.querySelector("#compose-subject").value;
+  const content = document.querySelector("#compose-body").value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: content
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      console.log(result);
+      load_mailbox('sent');
+  })
+  .catch(error => {
+      console.log('Error:', error);
+  });
 }
