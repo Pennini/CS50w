b938be2974a8fccb6132b616bd7e591f49f370be
Andre <andrepennini@gmail.com>
2023-11-06 18:42:04 -0300

    only reply button missing

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -7,6 +7,53 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#compose').addEventListener('click', compose_email);
 
   document.querySelector('#compose-form').addEventListener('submit', sendEmail);
+  window.addEventListener('click', (event) => {
+    const element = event.target;
+    if (element.className === 'emails-list' || element.tagName === 'SPAN' || element.tagName === 'STRONG') {
+      const emails = document.querySelector("#emails-view");
+      const id_mail = element.dataset.id;
+      fetch(`/emails/${id_mail}`)
+      .then(response => response.json())
+      .then(email => {
+        emails.innerHTML = '';
+        // ... do something else with email ...
+        const div = document.createElement('div');
+        div.innerHTML = `<strong>From:</strong> ${email['sender']}<br><strong>To:</strong> ${email['recipients']}<br><strong>Subject:</strong> ${email['subject']}<br><strong>Timestamp:</strong> ${email['timestamp']}<br><br><button class="btn btn-sm btn-outline-primary" id="reply">Reply</button> <hr> <br> ${email['body']}`;
+        emails.appendChild(div);
+        fetch(`/emails/${id_mail}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+              read: true
+          })
+        })
+      });
+    }
+    if (element.dataset.name === 'archive') {
+      const id_mail = element.dataset.id;
+      var parentNode = element.parentNode.parentNode;
+      var is_archive = NaN;
+      if (parentNode.dataset.archive === 'true') {
+        is_archive = false;
+      } else {
+        is_archive = true;
+      }
+      fetch(`/emails/${id_mail}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: is_archive
+        })
+      })
+      .then(() => {
+        parentNode.style.animationPlayState = 'running';
+        parentNode.addEventListener('animationend', () => {
+          parentNode.remove();
+        });
+      })
+      .then(() => {
+        load_mailbox('inbox');
+      });
+    }
+  })
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -26,28 +73,40 @@ function compose_email() {
 
 function load_mailbox(mailbox) {
   
-  const emails = document.querySelector('#emails-view');
+  const views = document.querySelector('#emails-view');
   // Show the mailbox and hide other views
-  emails.style.display = 'block';
+  views.style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
   // Show the mailbox name
-  emails.innerHTML += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-  const list = document.createElement("ul");
-  list.className = "list-group";
-  emails.append(list);
+  views.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-      // Print emails
-      console.log(emails);
       // ... do something else with emails ...
       for (var i = 0; i < emails.length; i++) {
-        const li = document.createElement('li');
-        li.className = 'list-group-item';
-        li.innerHTML = `<span> id="email${i}" ${emails[i].sender}</span> <span class="subject">${emails[i].subject}</span> <span class="timestamp">${emails[i].timestamp}</span>`;
-        list.append(li);
+        const div = document.createElement('div');
+        div.dataset.id = `${emails[i].id}`
+        div.dataset.archive = `${emails[i].archived}`
+        div.className = 'emails-list';
+
+        if (emails[i].read) {
+          div.style.backgroundColor = 'gainsboro';
+        } else {
+          div.style.backgroundColor = 'white';
+        }
+
+        div.innerHTML = `<span data-id="${emails[i].id}"><strong data-id="${emails[i].id}">${emails[i].sender}</strong></span> <span data-id="${emails[i].id}" class="vl"></span> <span data-id="${emails[i].id}" class="subject">Subject: ${emails[i].subject}</span> <span data-id="${emails[i].id}" class="timestamp">${emails[i].timestamp}</span>`;
+
+        if (mailbox === 'inbox') {
+          div.innerHTML += `<span class="archive"><button data-name="archive" data-id="${emails[i].id}" class="btn btn-outline-primary">Archive</button></span>`
+        } else if (mailbox === 'archive') {
+          div.innerHTML += `<span class="archive"><button data-name="archive" data-id="${emails[i].id}" class="btn btn-outline-primary">Unarchive</button></span>`
+        } else {
+          div.innerHTML += `<span class="archive" style="visibility: hidden;"><button data-name="archive" data-id="${emails[i].id}" class="btn btn-outline-primary">Unarchive</button></span>`
+        }
+
+        views.appendChild(div);
       }
   });
 
