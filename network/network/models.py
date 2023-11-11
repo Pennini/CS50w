8bd16ffb3e39f1936fd1de61e6fec46d03ef6fa4
André Pennini <andrepennini@usp.br>
2023-11-11 19:32:57 -0300

    network models

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(max_length=280)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.content}"
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers"
+    )
+
+    def __str__(self):
+        return f"{self.user} follows {self.following}"
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    like = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} likes {self.post}"
